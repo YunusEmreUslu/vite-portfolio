8624c4a1851b3faa7a29946d69d715014dd1706d
YunusEmreUslu <usluyunusemre00@gmail.com>
2023-08-18 23:52:30 +0300

    first part

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='main'>
+        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src='https://images.unsplash.com/photo-1620651840447-0515d1cdf7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1175&q=80' alt=''></img>
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/10'>
+        <div>
+            <h1> I'm Yunus Emre Uslu</h1>
+            <h2> I'm</h2>
+        </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Main
